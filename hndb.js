e749860600b342525293e537486052038c06baa9
morpher <git@git.vrai.me>
2017-08-13 11:38:41 +0300

    Initial commit; sign up route implemeneted

--- hndb.js
+++ hndb.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 let userSchema = new mongoose.Schema({
-  name: String,
-  pwd: String,
+  name: { type: String, index: {unique: true}, required: true },
+  pwdhash: { type: String, required: true },
   karma: {type: Number, min: 0}
-});
+}, {timestamps: true});
 
 
 let postSchema = new mongoose.Schema({
-  title: { type: String, required: true},
-  text: { type: String, required: true},
-  userId: {type: mongoose.Schema.Types.ObjectId, ref: "User"}
-});
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  voters: [{ type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+  points: { type: Number, default: 0 }, // size of voters array
+  userId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}
+}, {timestamps: true});
 
 
 
--- server.js
+++ server.js
@@ -1,88 +1,60 @@
 const app             = require('express')();
 const bodyParser      = require('body-parser');
 const mongoose        = require('mongoose');
-const cookieParser    = require('cookie-parser');
-const session         = require('express-session');
+const bcrypt          = require('bcrypt');
 mongoose.Promise      = require('bluebird');
 const hndb            = require('./hndb');
 
+
 const passport        = require('passport');
+const BasicStrategy   = require('passport-http').BasicStrategy;
 
-/* Http digest auth 
-const DigestStrategy  = require('passport-http').DigestStrategy;
-*/
+process.on('uncaughtException', err=>{
+  console.log(`From process: ${err}`);
+});
 
-const LocalStrategy   = require('passport-local').Strategy;
+let User, Post;
 
-let userModel, postModel;
+passport.use( new BasicStrategy( (username, password, cb)=>{
+  (async function() {
+    try {
+      let user = await User.findOne({name: username});
+      if ( !user ) 
+        return cb(null, false );
+      let isEq = await bcrypt.compare( password, user.pwdhash );
+      cb( null, isEq ? user: false );
+    }
 
+    catch( err ) {
+      cb(err);
+    }
+  })();
 
-/* Http digest auth
- * curl -vv --user test:testpass --digest -H "Content-type: application/json" -X POST -d '{"title":"my title", "text":"test"}' http://localhost:8080/submit
- 
-passport.use( new DigestStrategy( 
-  { qop: "auth" }, 
-  function( username, cb ) {
-    console.log(`got user: ${username}`);
-    cb( null, username, 'testpass' );
-  })
-);
-*/
-
-let testUser = {
-  id: 1244,
-  name: 'pizdec'
-};
-
-passport.use( new LocalStrategy( function( username, password, cb) {
-  console.log(`Local auth`);
-  return cb(null, testUser );
 }));
-
-passport.serializeUser( (user, cb)=>{
-  cb(null, user.id);
-});
-
-passport.deserializeUser( (id, cb)=>{
-  cb(null, testUser );
-});
 
 
 app.use( bodyParser.json());
-app.use( bodyParser.urlencoded({extended: true}) );
-app.use( session( { secret: 'nanit', resave: false, saveUninitialized: false } ) );
-app.use( passport.initialize() );
-app.use( passport.session() );
 
-const ConnStr     = "mongodb://localhost/hn";
-const APP_PORT    = 8080;
+const ConnStr     = "mongodb://mongodb/hn";
+//const ConnStr       = "mongodb://localhost/hn";
+const APP_PORT      = 8080;
 
-app.post('/login', passport.authenticate('local'), (req,res)=>{
-  res.end();
-});
-
-//app.post('/submit', passport.authenticate('digest', { session: false }), (req,res)=>{
-app.post('/submit', (req,res)=>{
+app.post('/signup', (req,res)=>{
   (async function() {
     try {
-      await post.create( req.body );
-      console.log(`Wrote to db: ${JSON.stringify( req.body )}`);
-      res.status(200).end();
+      let { password: pwd, username: name }  = req.body;
+      if ( pwd && pwd.length < 8 ) 
+        return res.json( { status: false, result: 'password should be at least 8 chars'});
+      if ( name && name.length < 4 ) 
+        return res.json( { status: false, result: 'username should be at least 4 chars'});
+      let pwdhash = await bcrypt.hash( pwd, 10 );
+      let result = await User.create( { name: name, pwdhash: pwdhash } );
+      res.json( { status: true, result: result} );
     }
-    catch( err ) {
-      console.log(`Got err: ${err.message}`);
-      res.status('500').end();
-    }
-  })();    
-});
-
-app.put('/edit', (req,res)=>{
-  let pid = req.query.id;
-  res.send('ok'); 
-           
-});
-
-app.get('/newest', (req,res)=>{
+    catch(err) {
+      res.json({ status: false, result: err.message} );
+    } 
+  })();
   
 });
 
@@ -90,8 +62,8 @@ app.get('/newest', (req,res)=>{
 mongoose
 .connect(ConnStr, { useMongoClient: true })
 .then( db=>{
-  userModel = db.model('user', hndb.userSchema );
-  postModel = db.model('post', hndb.postSchema );
+  User = db.model('User', hndb.userSchema );
+  Post = db.model('Post', hndb.postSchema );
   app.listen(APP_PORT);
   console.log(`Listening on ${APP_PORT}`);
 })
